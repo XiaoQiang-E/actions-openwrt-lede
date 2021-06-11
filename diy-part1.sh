2b734d6076b2ff0b8f528d14a606a5dc1db9c78d
XiaoQiang-E <77094200+XiaoQiang-E@users.noreply.github.com>
2021-06-11 11:51:40 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,7 @@
 
 # Add a feed source
 sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+git clone https://github.com/KyleRicardo/MentoHUST-OpenWrt-ipk.git package/MentoHUST-OpenWrt-ipk
+git clone https://github.com/BoringCat/luci-app-mentohust.git package/luci-app-mentohust
+git clone https://github.com/pymumu/smartdns.git package/smartdns
